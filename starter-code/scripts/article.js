f6a77840a079887cef5306ea346bce7bd8776edc
Kyle Richey <richey.kyle.i@gmail.com>
2017-07-27 20:45:32 -0700

    finished all TODO

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -5,6 +5,12 @@ var articles = [];
 function Article (rawDataObj) {
   // TODO: Use the JS object passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
+  this.title = rawDataObj.title;
+  this.category = rawDataObj.category;
+  this.author = rawDataObj.author;
+  this.authorUrl = rawDataObj.authorUrl;
+  this.publishedOn = rawDataObj.publishedOn;
+  this.body = rawDataObj.body;
 }
 
 Article.prototype.toHtml = function() {
@@ -13,6 +19,7 @@ Article.prototype.toHtml = function() {
   However, in our modules.css stylesheet, we gave all elements
   with a class of template a display of none. Let's make
   sure we're not accidentally hiding our cloned article! */
+  $newArticle.removeAttr('class');
 
   if (!this.publishedOn) $newArticle.addClass('draft');
   $newArticle.data('category', this.category);
@@ -26,6 +33,13 @@ Article.prototype.toHtml = function() {
     3. article title,
     4. article body, and
     5. publication date. */
+
+    $newArticle.find('h1').text(this.title);
+    $newArticle.find('article').attr('data-category', this.category);
+
+    $newArticle.find('a').attr('href', this.authorUrl);
+
+    $newArticle.find('.article-body').html(this.body);
 
     $newArticle.find('.byline a').html(this.author);
 
